bd13df3fa4e845185aef15391bb41d1d48279d91
sleeping-psystudent <b08207042@g.ntu.edu.tw>
2024-06-22 10:55:16 +0800

    SQL revise, add progress in store, interface revise

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 import gradio as gr
-import pandas as pd
 import process
+import datetime
+import lib
+
+# source: https://www.cnblogs.com/tian777/p/17371619.html
+def store(progress = gr.Progress()):
+    progress(0, desc="Starting")
+    time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    data = [{'cr_cono': time,
+    'cr_clas': '',
+    'cr_cnam': '',
+    'cr_tenam': '',
+    'cr_no': '',
+    'cr_time': ''}]
+
+    sem = lib.latest_semester()
+    clrooms = lib.class_rooms()
+    for clroom in progress.tqdm(clrooms, desc="Reversing"):
+        url = "https://gra206.aca.ntu.edu.tw/classrm/acarm/webcr-use-new?SYearDDL="+sem+"&BuildingDDL=%25&RoomDDL="+clroom+"&SelectButton=%E6%9F%A5%E8%A9%A2"
+        data += lib.crawler(url)
+    process.SQL_storage(data)
 
 def main():
     with gr.Blocks() as demo:
@@ -11,10 +30,13 @@ def main():
                 day_input = gr.Dropdown(choices=["不限", "星期一", "星期二", "星期三", "星期四", "星期五"], label="時間", value="不限")
                 submit_button = gr.Button("Submit")
                 time_output = gr.Textbox(label="資料集更新時間")
+                update_button = gr.Button("Update")
+                progress_output = gr.Textbox(label="資料集更新進度")
             with gr.Column(scale = 2):
                 dataframe_output = gr.HTML(label="搜尋結果")
 
         submit_button.click(process.generate, inputs=[course_name_input, teacher_name_input, day_input], outputs=[time_output, dataframe_output])
+        update_button.click(store, inputs=[], outputs=[progress_output])
     demo.launch(share=True, debug=True)
 
 if __name__=='__main__':
--- process.py
+++ process.py
@@ -1,12 +1,13 @@
 import sqlite3
 import pandas as pd
-import datetime
-import lib
 
 def SQL_storage(data):
     # 連接SQLite數據庫
     conn = sqlite3.connect('classrooms.db')
     cursor = conn.cursor()
+
+    # 清空檔案
+    cursor.execute('truncate table history')
 
     # 創建表格
     cursor.execute('''
@@ -37,22 +38,6 @@ def load():
     db = sqlite3.connect("classrooms.db")
     data = pd.read_sql_query(query, db)
     return data
-
-def store():
-    time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    data = [{'cr_cono': time,
-    'cr_clas': '',
-    'cr_cnam': '',
-    'cr_tenam': '',
-    'cr_no': '',
-    'cr_time': ''}]
-
-    sem = lib.latest_semester()
-    clrooms = lib.class_rooms()
-    for clroom in clrooms:
-        url = "https://gra206.aca.ntu.edu.tw/classrm/acarm/webcr-use-new?SYearDDL="+sem+"&BuildingDDL=%25&RoomDDL="+clroom+"&SelectButton=%E6%9F%A5%E8%A9%A2"
-        data += lib.crawler(url)
-    SQL_storage(data)
 
 def generate(course_name, teacher_name, selected_day):
     result = load()
